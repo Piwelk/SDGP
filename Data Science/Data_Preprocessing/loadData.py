96159c095d6de05df5e1618f5a4647f75d6164f8
Piwelk <piumi.2017592@iit.ac.lk>
2020-03-01 20:14:14 -0800

    Added load data and sentiment

--- Data Science/Data_Preprocessing/loadData.py
+++ Data Science/Data_Preprocessing/loadData.py
@@ -11,6 +11,7 @@ for fileName in os.listdir(directory):
     f.close()
 
 dataset.sort_values(by=['id'], inplace=True)
+dataset = dataset.reset_index()
 print(dataset.head())
 
 
@@ -23,10 +24,50 @@ with open('../../../aclImdb/train/urls_neg.txt') as f1:
 urlFrame = pd.DataFrame({'url' : url }) 
 print(urlFrame.head())
 
+#pd.merge(dataset,urlFrame,on='')
 finalDataSet = pd.concat([dataset, urlFrame], axis=1)
+finalDataSet['movieName'] = finalDataSet['url'].str[26:35]
+finalDataSet['isNeg'] = 1
 print(finalDataSet)
 
-    #print(str(fileName)[2:-1])
-    #f = open('../../../aclImdb/train/neg/'+str(fileName)[2:-1], 'r')
-    #print(f.read())
-    #f.close
+#----------------------------------------------------------------------------------------------------------------------------------------------------
+
+datasetPos = pd.DataFrame()
+directoryPos = os.fsencode('../../../aclImdb/train/pos/')
+
+for fileNamePos in os.listdir(directoryPos):
+    with open('../../../aclImdb/train/pos/'+str(fileNamePos)[2:-1]) as f:
+        review = f.read()
+    datasetPos = pd.concat([datasetPos,pd.DataFrame(data={'filename': [str(fileNamePos)[2:-1]], 'review' : [review], 'id' : [int(str(fileNamePos)[2:str(fileNamePos).index('_')])]})])
+    f.close()
+
+datasetPos.sort_values(by=['id'], inplace=True)
+datasetPos = datasetPos.reset_index()
+print(datasetPos.head())
+
+
+urlPos = []
+
+with open('../../../aclImdb/train/urls_pos.txt') as f1:
+    for line in f1:
+        urlPos.append(line)
+
+urlFramePos = pd.DataFrame({'url' : urlPos }) 
+print(urlFramePos.head())
+
+#pd.merge(dataset,urlFrame,on='')
+finalDataSetPos = pd.concat([datasetPos, urlFramePos], axis=1)
+finalDataSetPos['movieName'] = finalDataSetPos['url'].str[26:35]
+finalDataSetPos['isNeg'] = 0
+print(finalDataSetPos)
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------
+
+trainingDataset = finalDataSet.append(finalDataSetPos)
+print(trainingDataset)
+
+if os.path.exists('../Data/Training_Data/Train.csv'):
+    os.remove('../Data/Training_Data/Train.csv')
+else:
+    trainingDataset.to_csv(r'../Data/Training_Data/Train.csv')
+    print('data written to file in ../Data/Training_Data/Train.csv')
